e9af7ce75d10d0751e1789933b16efc3b6e30c41
Nina.Nuss <maximnuss120697@gmail.com>
2026-04-22 21:42:00 +0200

    .

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,22 @@ import { neon } from '@neondatabase/serverless';
 import * as schema from './schema/schema.ts';
 import { env } from '$env/dynamic/private';
 
+
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = neon(env.DATABASE_URL);
 
+console.log('Connecting to database...');
+
 export const db = drizzle(client, { schema });
+
+
+db.select().from(schema.task).then(users => {
+    console.log('Connected to database successfully!');
+    console.log('Existing tasks:', users);
+}).catch(error => {
+    console.error('Error connecting to database:', error);
+});
+
+
+export * from './schema/schema.ts';
